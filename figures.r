92dcd1329b91ee994e03976a31597556893c0d5f
jwingenroth <jwing421@gmail.com>
2023-11-27 17:27:09 -0500

    Improve fire data reformatting

--- figures.r
+++ figures.r
@@ -7,13 +7,24 @@ library(stars)
 #### Load and format data
 
 ### Owl data
+
+# Used a .tif I produced earlier because processing original data was slow
 owl_data <- read_stars("./output/owl_habitat_quality_near_Rim.tif")
 
 ### Fire data
-fires <- st_read("./data/CA_fire_perimeters/California_Fire_Perimeters__all_.shp") 
-rim <-  fires %>%
-    filter(FIRE_NAME == "RIM", YEAR_ == 2013) %>%
-    st_transform(3310) # Convert to lat/long
+
+# Load fire data
+all_fires <- st_read("./data/CA_fire_perimeters/California_Fire_Perimeters__all_.shp") 
+
+# Isolate Rim Fire
+rim_fire <- filter(all_fires, FIRE_NAME == "RIM", YEAR_ == 2013)
+
+# Find fires near Rim Fire
+nearby_fires <- st_crop(all_fires, st_bbox(rim_fire), epsilon = 1.1)
+
+# Match to owl data projection (https://epsg.io/3310)
+rim_proj <- st_transform(rim_fire, 3310)
+nearby_proj <- st_transform(nearby_fires, 3310)
 
 #### Plot figures
 
@@ -21,9 +32,9 @@ rim <-  fires %>%
 
 ### Figure 3
 
-p1 <- ggplot() +
-    geom_stars(data = st_crop(owl_data, st_bbox(rim), epsilon = 1.1)) +
-    geom_sf(aes(color = ""), data = rim, fill = NA, linewidth = 1.1) +
+p3 <- ggplot() +
+    geom_stars(data = st_crop(owl_data, st_bbox(rim_proj), epsilon = 1.1)) +
+    geom_sf(aes(color = ""), data = rim_proj, fill = NA, linewidth = 1.1) +
     theme_bw() +
     scale_fill_viridis_c(option = "mako", begin = 0.1) +
     scale_color_manual(values = "red") +
@@ -34,6 +45,6 @@ p1 <- ggplot() +
         y = "Latitude"
     )
 
-ggsave("figures/Figure_3.svg", p1)
+ggsave("figures/Figure_3.svg", p3)
 
 ### Figure 4
