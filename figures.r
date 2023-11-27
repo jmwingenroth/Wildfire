fcc4c9f66184cc98c38ca67304715efa46f632ac
jwingenroth <jwing421@gmail.com>
2023-11-27 16:53:04 -0500

    Update Figure 3

--- figures.r
+++ figures.r
@@ -8,42 +8,30 @@ library(stars)
 
 ### Owl data
 
-# Load attribute table that keys values to point IDs
-owl_vat <- st_read("./data/owl_raster/ds2185.gdb", layer = "VAT_ds2185")
-
-# Load shapefile with point IDs, join attribute table, and reformat
-owl_data <- st_read("./data/owl_raster/owl_raster_key.shp") %>%
-    filter(row_number()%%9 == 0) %>% # Lower resolution to speed up code
-    right_join(tibble(owl_vat), by = c("VALUE" = "Value")) %>%
-    filter(NAME == "SPOTTED OWL") %>%
-    select(quality = MEAN, geometry) %>%
-    st_transform(4326) # Convert to lat/long
-
-# Drop missing values
-owl_clean <- owl_data[!st_is_empty(owl_data),,drop = FALSE]
-
-# Rasterize owl data
-owl_raster <- st_rasterize(owl_clean)
-owl_raster[is.na(owl_raster[])] <- 0 # Set NA values to zero
+owl_data <- read_stars("./output/owl_habitat_quality_near_Rim.tif")
 
 ### Fire data
 
-fires <- st_read("./data/CA_fire_perimeters/California_Fire_Perimeters__all_.shp") %>%
-    st_transform(4326) # Convert to lat/long
-
-rim <- fires %>%
-    filter(FIRE_NAME == "RIM", YEAR_ == 2013)
-
+rim <- st_read("./data/CA_fire_perimeters/California_Fire_Perimeters__all_.shp") %>%
+    filter(FIRE_NAME == "RIM", YEAR_ == 2013) %>%
+    st_transform(3310) # Convert to lat/long
 
 #### Figure 2
 
 #### Figure 3
 
 p1 <- ggplot() +
-    geom_stars(data = st_crop(owl_raster, st_bbox(rim))) +
-    geom_sf(data = rim, fill = NA, color = "white", linewidth = 1) +
+    geom_stars(data = st_crop(owl_data, st_bbox(rim))) +
+    geom_sf(aes(color = ""), data = rim, fill = NA, linewidth = 1) +
     theme_bw() +
-    scale_fill_viridis_c()
+    scale_fill_viridis_c(option = "mako", begin = 0.1) +
+    scale_color_manual(values = "red") +
+    labs(
+        fill = "Spotted owl\nhabitat quality",
+        color = "Rim Fire perimeter",
+        x = "Longitude",
+        y = "Latitude"
+    )
 
 ggsave("figures/habitat_and_Rim.svg", p1)
 
