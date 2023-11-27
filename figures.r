0573d04585dd03a352bb93654208537944b7d1d6
jwingenroth <jwing421@gmail.com>
2023-11-27 13:09:43 -0500

    Add a first draft of Figure 3

--- figures.r
+++ figures.r
@@ -8,40 +8,43 @@ library(stars)
 
 ### Owl data
 
-# Load data keying values to point IDs
+# Load attribute table that keys values to point IDs
 owl_vat <- st_read("./data/owl_raster/ds2185.gdb", layer = "VAT_ds2185")
 
 # Load shapefile with point IDs, join attribute table, and reformat
-owl_tidy <- st_read("./data/owl_raster/owl_raster_key.shp") %>%
-    left_join(tibble(owl_vat), by = c("VALUE" = "Value")) %>%
+owl_data <- st_read("./data/owl_raster/owl_raster_key.shp") %>%
+    filter(row_number()%%9 == 0) %>% # Lower resolution to speed up code
+    right_join(tibble(owl_vat), by = c("VALUE" = "Value")) %>%
     filter(NAME == "SPOTTED OWL") %>%
     select(quality = MEAN, geometry) %>%
     st_transform(4326) # Convert to lat/long
 
-# Rasterize owl data
-owl_raster <- st_rasterize(owl_tidy)
-owl_raster[is.na(owl_raster[])] <- 0 
+# Drop missing values
+owl_clean <- owl_data[!st_is_empty(owl_data),,drop = FALSE]
 
-# Fire data
+# Rasterize owl data
+owl_raster <- st_rasterize(owl_clean)
+owl_raster[is.na(owl_raster[])] <- 0 # Set NA values to zero
+
+### Fire data
 
 fires <- st_read("./data/CA_fire_perimeters/California_Fire_Perimeters__all_.shp") %>%
-    st_transform(4326)
+    st_transform(4326) # Convert to lat/long
 
 rim <- fires %>%
     filter(FIRE_NAME == "RIM", YEAR_ == 2013)
 
-p1 <- ggplot() +
-    geom_stars(data = st_crop(owl_raster, st_bbox(rim))) +
-    geom_sf(data = rim, fill = NA, color = "white", linewidth = 1) +
-    theme_bw()
-
-ggsave("figures/habitat_and_Rim.svg", p1)
 
 #### Figure 2
 
 #### Figure 3
 
-ggplot() +
-    geom_stars(data = owl_raster)
+p1 <- ggplot() +
+    geom_stars(data = st_crop(owl_raster, st_bbox(rim))) +
+    geom_sf(data = rim, fill = NA, color = "white", linewidth = 1) +
+    theme_bw() +
+    scale_fill_viridis_c()
+
+ggsave("figures/habitat_and_Rim.svg", p1)
 
 #### Figure 4
