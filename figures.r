b60c0c4c9e0ce0a16cfe35e1cc66303d5239786e
jwingenroth <jwing421@gmail.com>
2023-11-29 14:31:59 -0500

    Improve color scale for vegetation map

--- figures.r
+++ figures.r
@@ -61,14 +61,22 @@ veg_tidy <- as_tibble(bind_cols(as.data.frame(veg_data), veg_coords)) %>%
     filter(!is.na(EVT_PHYS)) %>%
     mutate(veg_cats = fct_collapse(
         EVT_PHYS,
-        Developed = c(
+        `Developed and Agricultural` = c(
             "Developed",
             "Developed-Roads",
             "Developed-Low Intensity",
             "Developed-Medium Intensity",
             "Quarries-Strip Mines-Gravel Pits",
-            "Developed-High Intensity"
-        )
+            "Developed-High Intensity",
+            "Agricultural",
+            "Exotic Herbaceous"
+        ),
+        `Sparsely Vegetated and Barren` = c(
+            "Barren",
+            "Sparsely Vegetated",
+            "Snow-Ice"
+        ),
+        Conifer = c("Conifer", "Conifer-Hardwood")
     ))
 
 #### Plot figures
@@ -79,7 +87,18 @@ p2 <- ggplot() +
     geom_raster(data = veg_tidy, aes(x = x, y = y, fill = veg_cats)) +
     geom_sf(aes(color = ""), data = rim_fire, fill = NA, linewidth = 0.8) +
     theme_bw() +
-#    scale_fill_viridis_c(option = "mako", begin = 0.1) +
+    scale_fill_manual(
+        values = c(
+            "pink",
+            "#f5dc6e",
+            "dark green",
+            "cyan",
+            "green",
+            "dark blue",
+            "light blue",
+            "#3ea1b3"
+        )
+    ) +
     scale_color_manual(values = "red") +
     scale_x_continuous(expand = c(0,0), limits = figure_bbox[c("xmin","xmax")]) +
     scale_y_continuous(expand = c(0,0), limits = figure_bbox[c("ymin","ymax")]) +
