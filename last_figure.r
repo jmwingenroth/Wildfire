671ae96d0824563085aa772580379157e908a674
jwingenroth <jwing421@gmail.com>
2023-12-05 16:57:55 -0500

    Complete Figure 4b

--- last_figure.r
+++ last_figure.r
@@ -7,10 +7,6 @@ library(tidyverse)
 library(sf)
 library(raster)
 
-# I'm not sure whether this script needs these
-library(foreign)
-library(ggnewscale)
-
 sf_use_s2(FALSE)
 
 ### Load fire, treatment, and NP data
@@ -29,6 +25,12 @@ figure_bbox <- bb_shrink(st_bbox(rim_fire), e = -0.1)
 nearby_fires <- st_crop(all_fires, figure_bbox) %>%
     filter(FIRE_NAME != "RIM", YEAR_ %in% 1993:2013) %>%
     arrange(desc(SHAPE_Area))
+
+# Clip to Rim Fire area and get large areas of overlap
+large_fires_within <- st_intersection(nearby_fires, rim_fire) %>%
+    transmute(area = as.numeric(st_area(geometry))) %>%
+    filter(area > 4e6) %>%
+    st_union()
 
 # Load treatment data and project to WGS 84
 treatments <- read_sf("./data/treatment_data/Rim_Rire_Old_Proj.shp") %>%
@@ -62,11 +64,15 @@ burn_coords <- xyFromCell(burn_data, seq_len(ncell(burn_data)))
 burn_tidy <- as_tibble(bind_cols(as.data.frame(burn_data), burn_coords)) %>%
     filter(!is.na(Layer_1))
 
-ggplot() +
+burn_overlay <- ggplot() +
     geom_raster(data = burn_tidy, aes(x = x, y = y, fill = Layer_1)) +
     geom_sf(data = rim_fire, fill = NA, color = "black", linewidth = 0.8) +
     geom_sf(data = yosemite_within, fill = NA, color = "black", linewidth = 0.8, lty = "31") +
+    geom_sf(aes(color = " "), data = large_fires_within, fill = NA, linewidth = 1.3) +
     theme_bw() +
-    scale_fill_viridis_c(option = "turbo", begin = .2) +
-    theme(legend.position = "bottom") +
-    labs(fill = "Burn severity (dNBR)")
+    scale_fill_viridis_c(option = "turbo", begin = .5,) +
+    scale_color_manual(values = "blue") +
+    labs(fill = "Burn Severity (dNBR)", color = "Largest Subregions\nBurned Since 1993", x = "", y = "")
+
+ggsave("figures/Figure_4b_overlay.svg", burn_overlay, height = 7, width = 7)
+ggsave("figures/Figure_4b_overlay.png", burn_overlay, height = 7, width = 7, dpi = 600)
